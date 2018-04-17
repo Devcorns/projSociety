1d202e2aa3bf2d73b2d8207ffb86b9e03d89974c
Prakhar mathur <itprakhar@gmail.com>
2018-04-17 23:11:30 +0530

    try commit

--- server-api/app.js
+++ server-api/app.js
@@ -19,4 +19,4 @@ app.get('/mongo', function(req, res) {
 })
 
 
-app.listen(3001, () => console.log('Example app listening on port 3001!'))
+app.listen(process.env.PORT || 3001, () => console.log('Example app listening on port 3001!'))
