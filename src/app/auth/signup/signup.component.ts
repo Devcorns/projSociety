c70a80fc77d7cb78210565bd702ba4dff25cf512
Prakhar mathur <itprakhar@gmail.com>
2018-04-24 10:46:48 +0530

    Signup up Validation

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -19,8 +19,8 @@ export class SignupComponent implements OnInit {
     this.signupForm = formbuilder.group({
       email:new FormControl("",[Validators.required,Validators.minLength(6),Validators.pattern("^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$")]),
       mobile:new FormControl("",[Validators.required,Validators.minLength(10),Validators.maxLength(11),Validators.pattern('^[0-9]{10}$')]),
-       passwordOne:new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(20),]),
-       passwordRe:new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(20),PasswordValidationService.MatchPassword])
+       passwordOne:new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern('')]),
+       passwordRe:new FormControl("",[Validators.required,Validators.minLength(8),Validators.maxLength(20),Validators.pattern(''),PasswordValidationService.MatchPassword])
     });
 
     
